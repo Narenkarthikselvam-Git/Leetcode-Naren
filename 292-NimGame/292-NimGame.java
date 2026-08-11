fd8b4314c3434ed2fb945a7475ef58f403a565fd
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-08-11 23:03:35 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 11/08/2026, 23:03:35
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+        return false;
+        else
+        return true;
+        
+    }
+}

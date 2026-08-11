8dc8092d269089c75b98bda9331965e135f78659
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-08-11 23:02:33 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,27 @@
+// Last updated: 11/08/2026, 23:02:32
+class Solution {
+    public boolean judgeCircle(String moves) {
+
+        int x = 0;
+        int y = 0;
+
+        for(int i = 0; i < moves.length(); i++) {
+
+            char ch = moves.charAt(i);
+
+            if(ch == 'U')
+                y++;
+
+            else if(ch == 'D')
+                y--;
+
+            else if(ch == 'L')
+                x--;
+
+            else
+                x++;
+        }
+
+        return x == 0 && y == 0;
+    }
+}

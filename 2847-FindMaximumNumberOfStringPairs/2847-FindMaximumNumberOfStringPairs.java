ee20c481afd9e7da8fc1c25577ec6951c8828e7c
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-08-11 22:59:57 +0530

    Create 2847-FindMaximumNumberOfStringPairs

---
+++ 2847-FindMaximumNumberOfStringPairs/2847-FindMaximumNumberOfStringPairs.java
@@ -0,0 +1,20 @@
+// Last updated: 11/08/2026, 22:59:56
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+
+        int count = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+
+                if (words[i].charAt(0) == words[j].charAt(1) &&
+                    words[i].charAt(1) == words[j].charAt(0)) {
+
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}

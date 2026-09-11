da36f4a5d6fde087f607bd843b6ecf23b5201016
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-11 09:28:44 +0530

    Create 1518-WaterBottles

---
+++ 1518-WaterBottles/1518-WaterBottles.java
@@ -0,0 +1,13 @@
+// Last updated: 11/09/2026, 09:28:44
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int totalBottles = numBottles;
+
+        while (numBottles >= numExchange) {
+            totalBottles += numBottles / numExchange;
+            numBottles = (numBottles / numExchange) + (numBottles % numExchange);
+        }
+
+        return totalBottles;
+    }
+}

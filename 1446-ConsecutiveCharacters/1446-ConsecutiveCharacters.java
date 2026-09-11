a6de39d418b2a230ab72f689c2bf9995f385a8aa
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-11 09:28:45 +0530

    Create 1446-ConsecutiveCharacters

---
+++ 1446-ConsecutiveCharacters/1446-ConsecutiveCharacters.java
@@ -0,0 +1,18 @@
+// Last updated: 11/09/2026, 09:28:45
+class Solution {
+    public int maxPower(String s) {
+        int max = 1;
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+}

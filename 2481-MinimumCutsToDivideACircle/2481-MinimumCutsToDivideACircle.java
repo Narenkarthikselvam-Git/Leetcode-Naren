b119deb4cc3bf1ac4d8a1bbe096c3a5e34831672
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-11 09:27:53 +0530

    Create 2481-MinimumCutsToDivideACircle

---
+++ 2481-MinimumCutsToDivideACircle/2481-MinimumCutsToDivideACircle.java
@@ -0,0 +1,12 @@
+// Last updated: 11/09/2026, 09:27:52
+class Solution {
+    public int numberOfCuts(int n) {
+        if(n==1)
+        return 0;
+        else if(n%2!=0)
+        return n;
+        else
+        return n/2;
+        
+    }
+}

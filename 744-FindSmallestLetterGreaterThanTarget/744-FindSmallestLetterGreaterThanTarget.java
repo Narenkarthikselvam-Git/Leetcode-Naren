1be33d74caba01d81805087895bd7e987bed42f2
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-11 09:30:59 +0530

    Create 744-FindSmallestLetterGreaterThanTarget

---
+++ 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,11 @@
+// Last updated: 11/09/2026, 09:30:58
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] > target) {
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+}

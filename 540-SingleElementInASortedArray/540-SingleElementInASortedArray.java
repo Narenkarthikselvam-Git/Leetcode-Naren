2128157c7d6f4cb8587f239036a579a4fa736e9d
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-11 09:31:16 +0530

    Create 540-SingleElementInASortedArray

---
+++ 540-SingleElementInASortedArray/540-SingleElementInASortedArray.java
@@ -0,0 +1,26 @@
+// Last updated: 11/09/2026, 09:31:15
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // Make mid even
+            if (mid % 2 == 1) {
+                mid--;
+            }
+
+            // Pair is correct: nums[mid] == nums[mid + 1]
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } 
+            else {
+                high = mid;
+            }
+        }
+
+        return nums[low];
+    }
+}

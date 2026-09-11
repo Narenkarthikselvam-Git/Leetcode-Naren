a13700abcf729c65a127188f4583d23f2379d435
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-11 09:27:33 +0530

    Create 2651-CalculateDelayedArrivalTime

---
+++ 2651-CalculateDelayedArrivalTime/2651-CalculateDelayedArrivalTime.java
@@ -0,0 +1,6 @@
+// Last updated: 11/09/2026, 09:27:33
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return(arrivalTime+delayedTime)%24;
+    }
+}

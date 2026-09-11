41bf30ce7bef9ceb9d58a53516b7636d99148ed2
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-11 09:28:53 +0530

    Create 1342-NumberOfStepsToReduceANumberToZero

---
+++ 1342-NumberOfStepsToReduceANumberToZero/1342-NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,15 @@
+// Last updated: 11/09/2026, 09:28:53
+class Solution {
+    public int numberOfSteps(int num) {
+        int count=0;
+        while(num>0){
+            if(num%2==0){
+                num/=2;
+            }else{
+                num-=1;
+            }
+            count++;
+        }
+        return count;
+    }
+}

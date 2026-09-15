6c5e5b7b14814c895e6b66dde62473d8b1d17c27
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-15 10:37:32 +0530

    Create 151-ReverseWordsinaString

---
+++ 151-ReverseWordsinaString/version/java/151-ReverseWordsinaString_20260915_103731.java
@@ -0,0 +1,30 @@
+// Last updated: 15/09/2026, 10:37:31
+1class Solution {
+2    public String longestPalindrome(String s) {
+3        if (s == null || s.length() < 1) return "";
+4
+5        int start = 0, end = 0;
+6
+7        for (int i = 0; i < s.length(); i++) {
+8            int len1 = expandFromCenter(s, i, i);     
+9            int len2 = expandFromCenter(s, i, i + 1); 
+10            int len = Math.max(len1, len2);
+11
+12            if (len > end - start) {
+13                start = i - (len - 1) / 2;
+14                end = i + len / 2;
+15            }
+16        }
+17
+18        return s.substring(start, end + 1);
+19    }
+20
+21    private int expandFromCenter(String s, int left, int right) {
+22        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+23            left--;
+24            right++;
+25        }
+26        return right - left - 1; 
+27    }
+28}
+29

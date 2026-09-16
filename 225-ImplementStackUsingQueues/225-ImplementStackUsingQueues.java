7ed94a74b4f21108aa49e586a6d7cf6ec0b34770
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-16 10:08:57 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.java
@@ -0,0 +1,48 @@
+// Last updated: 16/09/2026, 10:08:56
+import java.util.*;
+
+class MyStack {
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    // Push element x onto stack
+    public void push(int x) {
+        q2.offer(x); // put new element in q2
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll()); // move all elements from q1 to q2
+        }
+        // swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    // Removes the element on top of the stack and returns it
+    public int pop() {
+        return q1.poll();
+    }
+
+    // Get the top element
+    public int top() {
+        return q1.peek();
+    }
+
+    // Returns whether the stack is empty
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */

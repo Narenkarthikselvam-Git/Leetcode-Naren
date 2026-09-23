5c473523f859b64d2e0f05daf418396a2b7ef1a4
Narenkarthikselvam-Git <narenkarthikselvam@gmail.com>
2026-09-23 09:12:39 +0530

    Create 844-BackspaceStringCompare

---
+++ 844-BackspaceStringCompare/844-BackspaceStringCompare.java
@@ -0,0 +1,51 @@
+// Last updated: 23/09/2026, 09:12:42
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            // Process backspaces in s
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            // Process backspaces in t
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            // Compare characters
+            if (i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)) {
+                return false;
+            }
+
+            // If one string is finished but the other is not
+            if ((i >= 0) != (j >= 0)) {
+                return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        return true;
+    }
+}
